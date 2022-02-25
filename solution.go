4c6feffe60f25f40e955458845d9dc1687cbec02
IliaBadz <badzaghua.ilia@gmail.com>
2022-02-25 11:37:32 +0400

    add: solution v2

--- solution.go
+++ solution.go
@@ -12,13 +12,14 @@ var SidesCircle intCustomType = 0
 
 func CalcSquare(sideLen float64, sidesNum intCustomType) float64 {
 	var square float64
-	if sidesNum == 3 {
+	switch {
+	case sidesNum == 3:
 		square = (math.Sqrt(3) / 4) * math.Pow(sideLen, 2)
-	} else if sidesNum == 4 {
+	case sidesNum == 4:
 		square = math.Pow(sideLen, 2)
-	} else if sidesNum == 0 {
+	case sidesNum == 0:
 		square = math.Pi * math.Pow(sideLen, 2)
-	} else {
+	default:
 		square = 0
 	}
 	return square
